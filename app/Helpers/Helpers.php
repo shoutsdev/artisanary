f82b09199aaf33b26971e74c29b1b34362e4f946
Tanvir Ahmed Hera <tahmedhera@gmail.com>
2023-04-21 20:39:49 +0600

    chapter 12 - Manage Role Permissions using Middleware

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -4,7 +4,7 @@ if (!function_exists('hasPermission')) {
 
     function hasPermission($key_word): bool
     {
-        if (in_array($key_word, auth()->user()->role->permissions)) {
+        if (in_array($key_word, auth()->user()->role->permissions) || auth()->user()->role_id == 1) {
             return true;
         }
         return false;
