a143a2d60b891d23e7d034d26b004b7c4520004b
Tanvir Ahmed Hera <tahmedhera@gmail.com>
2023-04-24 17:02:31 +0600

    chapter 16 - Testing with CRUD

---
+++ app/Http/Controllers/Api/BlogController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\BlogRequest;
+use App\Repositories\BlogRepository;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class BlogController extends Controller
+{
+    protected $blogRepository;
+
+    public function __construct(BlogRepository $blogRepository)
+    {
+        $this->blogRepository = $blogRepository;
+    }
+
+    public function index(): \Illuminate\Http\JsonResponse
+    {
+        try {
+            $data = [
+                'blogs' => $this->blogRepository->all()
+            ];
+            return response()->json($data);
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => $e->getMessage()
+            ]);
+        }
+    }
+
+    public function store(Request $request): \Illuminate\Http\JsonResponse
+    {
+        $validator = Validator::make($request->all(),[
+            'title'         => 'required|string|max:255|unique:blogs,title',
+            'description'   => 'required',
+            'image'         => 'required'
+        ]);
+
+        if($validator->fails()) {
+            return response()->json([
+                'error' => $validator->errors()
+            ]);
+        }
+
+        try {
+            $data = $request->all();
+            $data['user_id'] = 1;
+
+            $this->blogRepository->store($data);
+            return response()->json([
+                'success' => 'Blog created successfully'
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => $e->getMessage()
+            ]);
+        }
+    }
+    public function show(string $id): \Illuminate\Http\JsonResponse
+    {
+        try {
+            $data = [
+                'edit' => $this->blogRepository->find($id)
+            ];
+            return response()->json($data);
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => $e->getMessage()
+            ]);
+        }
+    }
+    public function update(Request $request, $id): \Illuminate\Http\JsonResponse
+    {
+        $validator = Validator::make($request->all(),[
+            'title'         => 'required|string|max:255|unique:blogs,title,'.$id,
+            'description'   => 'required'
+        ]);
+
+        if($validator->fails()) {
+            return response()->json([
+                'error' => $validator->errors()
+            ]);
+        }
+        try {
+            $this->blogRepository->update($id, $request->all());
+            return response()->json([
+                'success' => 'Blog updated successfully'
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => $e->getMessage()
+            ]);
+        }
+    }
+
+    public function destroy(string $id): \Illuminate\Http\JsonResponse
+    {
+        try {
+            $this->blogRepository->destroy($id);
+            return response()->json([
+                'success' => 'Blog deleted successfully'
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => $e->getMessage()
+            ]);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\BlogController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::resource('blogs', BlogController::class)->except(['edit','create','update']);
+Route::post('blogs/{id}', [BlogController::class, 'update'])->name('blogs.update');
